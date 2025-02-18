7be8cac485ea37ac31fbffdb9603e053c6db878d
Szymon <szymonnie2891@gmail.com>
2025-02-18 10:16:33 +0100

    bug fix

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const port = 3000;
 
 app.use(cors())
 
-const poll = mysql.createConnection(process.env.DB_URL
-//     {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     connectionLimit: 5
-// }
+const poll = mysql.createPool(
+    {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    connectionLimit: 5
+}
 );
 
 async function getQuestions(level) {   
