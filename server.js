6477ec0aae3692c3d640ed910cbc1594c3c0e46f
Szymon <szymonnie2891@gmail.com>
2025-02-18 10:18:58 +0100

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cors = require('cors');
 
 const app = express();
